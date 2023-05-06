4c97600947259ef1ef4d875a6854412723815520
Erik Olsson <erik.olsson@solidify.dev>
2023-05-06 19:41:26 +0200

    Added some more ansible playbook details, added keyvault in terraform, need to find out how to save and load secrets from keyvault next

--- main.tf
+++ main.tf
@@ -90,8 +90,8 @@ resource "azurerm_linux_virtual_machine" "examen-vm" {
 
   source_image_reference {
     publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "16.04-LTS"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts-gen2"
     version   = "latest"
   }
   depends_on = [
@@ -102,4 +102,38 @@ resource "azurerm_linux_virtual_machine" "examen-vm" {
 resource "local_file" "private_key" {
     content  = azurerm_public_ip.vm-external-ip.ip_address
     filename = "public_ip.txt"
+}
+
+
+
+
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_key_vault" "exam_key_vault" {
+  name                        = "ejoexamkeyvault"
+  resource_group_name         = azurerm_resource_group.examengroup.name
+  location                    = azurerm_resource_group.examengroup.location
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+
+  sku_name = "standard"
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    key_permissions = [
+      "Get", 
+    ]
+
+    secret_permissions = [
+      "Get", "Set"
+    ]
+
+    storage_permissions = [
+      "Get",
+    ]
+  }
 }
