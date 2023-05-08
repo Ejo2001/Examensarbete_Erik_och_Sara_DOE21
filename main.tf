f0ae9207a5f6afcf6b769986d415544e8a963e66
Erik Olsson <erik.olsson@solidify.dev>
2023-05-08 11:32:46 +0200

    Backup, working on terraform secrets

--- main.tf
+++ main.tf
@@ -109,6 +109,10 @@ resource "local_file" "public_ip_txt" {
 
 data "azurerm_client_config" "current" {}
 
+data "azuread_user" "user"{
+user_principal_name = "erik.olsson@solidify.dev"
+}
+
 resource "azurerm_key_vault" "exam_key_vault" {
   name                        = "ejoexamkeyvault"
   resource_group_name         = azurerm_resource_group.examengroup.name
@@ -122,18 +126,25 @@ resource "azurerm_key_vault" "exam_key_vault" {
 
   access_policy {
     tenant_id = data.azurerm_client_config.current.tenant_id
-    object_id = data.azurerm_client_config.current.object_id
-
-    key_permissions = [
-      "Get", 
-    ]
+    object_id = data.azuread_user.user.object_id
+    
 
     secret_permissions = [
-      "Get", "Set"
-    ]
-
-    storage_permissions = [
       "Get",
+      "List",
+      "Delete",
+      "Recover",
+      "Backup",
+      "Restore",
+      "Set",
     ]
   }
 }
+
+
+# Set a secret
+# resource "azurerm_key_vault_secret" "set_token" {
+#   name         = "secretpassword"
+#   value        = "secretpass!"
+#   key_vault_id = azurerm_key_vault.exam_key_vault.id
+# }
